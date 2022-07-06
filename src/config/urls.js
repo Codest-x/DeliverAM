2de01a91fd25401e528352da349526ce0bfd979b
Codest-x <davinsontc@outlook.com>
2022-07-06 17:36:17 -0500

    feat: add new endpoints

--- src/config/urls.js
+++ src/config/urls.js
@@ -8,3 +8,11 @@ export const LOGIN = getApiUrl('/auth/login');
 export const SIGNUP_CLIENT = getApiUrl('/auth/client/signup');
 export const SIGNUP_DOMICILIARY = getApiUrl('/auth/domiciliary/signup');
 export const USER_BY_TOKEN = getApiUrl('/auth');
+export const GET_ORDERS_BY_USER = getApiUrl('/orders/client/');
+export const SEND_DOMICILIARY_UBICATION = getApiUrl(
+  '/auth/domiciliary/ubications/',
+);
+
+export const GET_DOMICILIARYS_UBICATIONS = getApiUrl(
+  '/auth/domiciliary/ubications',
+);

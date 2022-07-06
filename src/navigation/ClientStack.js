88835fb7f4a43b2da26339ba70726563da36a661
Codest-x <davinsontc@outlook.com>
2022-07-06 17:34:28 -0500

    feat: Client Stack

---
+++ src/navigation/ClientStack.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {HomeC} from '../screens';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default function ClientStack(Drawer) {
+  return (
+    <>
+      <Drawer.Screen
+        name="Inicio"
+        component={HomeC}
+        options={{
+          headerShown: false,
+          /* drawerItemStyle: {
+                height: 0,
+              }, */
+          drawerIcon: ({color}) => (
+            <Icon name={'home-outline'} size={24} color={color} />
+          ),
+        }}
+      />
+    </>
+  );
+}

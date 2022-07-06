8ea190125b3216ad24e4e6ccf23201f05b1e5bfe
Codest-x <davinsontc@outlook.com>
2022-07-06 01:29:42 -0500

    fix: require permissions of user

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,15 @@
 import {SafeAreaView, ScrollView, StyleSheet, Text} from 'react-native';
 import React from 'react';
+import {useAuth} from '../contexts/auth';
 
 export default function Home({navigation}) {
+  const {authData} = useAuth();
   return (
     <SafeAreaView style={styles.HomeContainer}>
       <ScrollView contentContainerStyle={styles.HomeScroll}>
-        <Text style={{color: 'black'}}>Hola Como Estas</Text>
+        <Text style={{color: 'black'}}>
+          Bienvenido {authData?.user?.nombres} {authData?.user?.apellidos}
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );

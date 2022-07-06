6d28b5f64b1b2f11a0c5dca37757dc7ce55b1198
Codest-x <davinsontc@outlook.com>
2022-07-06 01:30:12 -0500

    fix: ignore NativeEventEmitter

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Remote debugger']);
+
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
 
 AppRegistry.registerComponent(appName, () => App);

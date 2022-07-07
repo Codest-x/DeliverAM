d1603841c95fae7c0e78964e3687c6b4834293ad
Codest-x <davinsontc@outlook.com>
2022-07-06 22:38:00 -0500

    fix: move RNL to context

--- App.js
+++ App.js
@@ -1,39 +1,19 @@
 import React from 'react';
 import Routes from './src/navigation/Route';
 import {AuthProvider} from './src/contexts/auth';
+import {LocationProvider} from './src/contexts/location';
 import {enableLatestRenderer} from 'react-native-maps';
-import RNLocation from 'react-native-location';
 
 enableLatestRenderer();
 
 // api key google maps  AIzaSyAjww-YnvLqmT-W4NWS-2OvpGa56CIHrcM
 
-RNLocation.configure({
-  distanceFilter: 50, // Meters
-  desiredAccuracy: {
-    ios: 'best',
-    android: 'balancedPowerAccuracy',
-  },
-});
-
-RNLocation.requestPermission({
-  ios: 'whenInUse',
-  android: {
-    detail: 'fine',
-    rationale: {
-      title: 'Necesitamos Acceder a tu ubicación',
-      message:
-        'Usaremos tu ubicacion para que las demas personas accedan a tu servicio',
-      buttonPositive: 'OK',
-      buttonNegative: 'Cancelar',
-    },
-  },
-});
-
 export default function App() {
   return (
     <AuthProvider>
-      <Routes />
+      <LocationProvider>
+        <Routes />
+      </LocationProvider>
     </AuthProvider>
   );
 }

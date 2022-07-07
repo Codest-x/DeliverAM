b97684eae9d789a6908c243145d58074d3f89d7a
Codest-x <davinsontc@outlook.com>
2022-07-06 22:37:00 -0500

    fix: import screen in index

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,5 +4,16 @@ import Login from './Login/Login';
 import SignUp from './SignUp/SignUp';
 import SignUpType from './SignUp/SignUpType';
 import OnboardingScreen from './OnBoarding/OnBoarding';
+import LoadingScreen from './LoadingScreen';
+import OfflineScreen from './OfflineScreen';
 
-export {HomeC, HomeD, Login, SignUp, SignUpType, OnboardingScreen};
+export {
+  HomeC,
+  HomeD,
+  Login,
+  SignUp,
+  SignUpType,
+  OnboardingScreen,
+  OfflineScreen,
+  LoadingScreen,
+};

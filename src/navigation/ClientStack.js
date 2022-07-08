a0dae83d657d1610e2caf50914f778a5688544e6
Codest-x <davinsontc@outlook.com>
2022-07-07 23:48:23 -0500

    fix: add addOrder Screen

--- src/navigation/ClientStack.js
+++ src/navigation/ClientStack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HomeC, ViewOrders} from '../screens';
+import {HomeC, ViewOrders, AddOrder} from '../screens';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function ClientStack(Drawer) {
@@ -28,6 +28,19 @@ export default function ClientStack(Drawer) {
               }, */
           drawerIcon: ({color}) => (
             <Icon name={'reader-outline'} size={24} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Agregar Orden"
+        component={AddOrder}
+        options={{
+          headerShown: false,
+          /* drawerItemStyle: {
+                height: 0,
+              }, */
+          drawerIcon: ({color}) => (
+            <Icon name={'add'} size={24} color={color} />
           ),
         }}
       />

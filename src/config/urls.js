97419bd0fecb5f0cb524537fbc0e0d21d223537f
Codest-x <davinsontc@outlook.com>
2022-07-07 23:49:04 -0500

    fix: add new endpoints

--- src/config/urls.js
+++ src/config/urls.js
@@ -4,11 +4,19 @@ export const getApiUrl = endpoint => {
   return `${API_URL}${endpoint}`;
 };
 
+/* Authentication */
 export const LOGIN = getApiUrl('/auth/login');
 export const SIGNUP_CLIENT = getApiUrl('/auth/client/signup');
 export const SIGNUP_DOMICILIARY = getApiUrl('/auth/domiciliary/signup');
 export const USER_BY_TOKEN = getApiUrl('/auth');
+
+/* Orders Service */
+
 export const GET_ORDERS_BY_USER = getApiUrl('/orders/client/');
+export const ADD_ORDER = getApiUrl('/orders');
+
+/* Auth Ubication */
+
 export const SEND_DOMICILIARY_UBICATION = getApiUrl(
   '/auth/domiciliary/ubications/',
 );

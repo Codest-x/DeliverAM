5f01093a075abcf207c400a8ae786353771fd44c
Codest-x <davinsontc@outlook.com>
2022-07-09 21:45:26 -0500

    feat: Home Domiciliary Design and functions

--- src/screens/DomiciliaryScreens/HomeD.js
+++ src/screens/DomiciliaryScreens/HomeD.js
@@ -1,18 +1,60 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+  Image,
+  StatusBar,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {theme} from '../../constants/theme';
 import {useAuth} from '../../contexts/auth';
 import {showError, showSuccess} from '../../utils/helperFunctions';
 import {sendUserUbication} from '../../services/authService';
 import {useLocation} from '../../contexts/location';
+import {getAllOrders} from '../../services/ordersService';
+import {useSocketIO} from '../../contexts/socketio';
+import OrderCard from '../../components/OrderCard';
 
-export default function HomeD() {
+export default function HomeD({navigation}) {
   const {authData, loading} = useAuth();
   const {locationData} = useLocation();
+  const {newOrder, deleteOrder} = useSocketIO();
+
+  const [orders, setOrders] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loadingOrders, setLoadingOrders] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    getAllOrders()
+      .then(orders => {
+        setOrders(orders);
+        setRefreshing(false);
+      })
+      .catch(err => {
+        showError('Error', err);
+      });
+  };
 
   useEffect(() => {
     !loading && authData && locationData && sendUserUbicationAsync();
   }, [locationData]);
+
+  useEffect(() => {
+    setLoadingOrders(true);
+    getAllOrders()
+      .then(orders => {
+        setOrders(orders);
+        setLoadingOrders(false);
+      })
+      .catch(err => {
+        showError('Error', err);
+      });
+  }, [newOrder, deleteOrder]);
 
   const sendUserUbicationAsync = async () => {
     locationData?.latitude && locationData?.longitude
@@ -36,19 +78,120 @@ export default function HomeD() {
   };
 
   return (
-    <View style={styles.HomeDomiciliary}>
-      <Text style={{color: theme.colors.primaryTextColor}}>
-        Bienvenido Eres un Domiciliario
-      </Text>
-    </View>
+    <SafeAreaView style={styles.HomeContainer}>
+      <ScrollView
+        contentContainerStyle={styles.HomeScroll}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={[theme.colors.primaryColor]}
+            progressBackgroundColor={theme.colors.accentColor}
+          />
+        }>
+        <View style={styles.OrdersContainer}>
+          {!loadingOrders && !refreshing ? (
+            orders.length > 0 ? (
+              orders.map(
+                order =>
+                  !order?.delivery && (
+                    <OrderCard
+                      key={order._id}
+                      data={order}
+                      delivery={true}
+                      onPress={() => {
+                        console.log(order?._id);
+                        navigation.navigate('Orden', {
+                          orderId: order?._id,
+                        });
+                      }}
+                    />
+                  ),
+              )
+            ) : (
+              <View style={styles.LoadinContainer}>
+                <Image
+                  source={require('../../assets/images/dont-move.gif')}
+                  style={{
+                    width: 300,
+                    height: 300,
+                    resizeMode: 'contain',
+                    marginBottom: -60,
+                  }}
+                />
+                <Text
+                  style={{
+                    color: 'black',
+                    fontWeight: 'bold',
+                    paddingVertical: 10,
+                    fontSize: 20,
+                    width: '90%',
+                    textAlign: 'center',
+                  }}>
+                  No tienes ordenes
+                </Text>
+              </View>
+            )
+          ) : (
+            <View style={styles.LoadinContainer}>
+              <Image
+                source={require('../../assets/images/dont-move.gif')}
+                style={{
+                  width: 300,
+                  height: 300,
+                  resizeMode: 'contain',
+                  marginBottom: -60,
+                }}
+              />
+              <Text
+                style={{
+                  color: 'black',
+                  fontWeight: 'bold',
+                  paddingVertical: 10,
+                  fontSize: 20,
+                  width: '90%',
+                  textAlign: 'center',
+                }}>
+                Estamos cargando tus ordenes se paciente si se demora en cargar
+                mas de lo normal reinicia la aplicación
+              </Text>
+              <ActivityIndicator
+                size="large"
+                color={theme.colors.accentColor}
+              />
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  HomeDomiciliary: {
+  HomeContainer: {
     flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    backgroundColor: theme.colors.primaryColor,
+  },
+  HomeScroll: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    marginTop: StatusBar.currentHeight,
+  },
+  OrdersContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  LoadinContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
   },
 });
